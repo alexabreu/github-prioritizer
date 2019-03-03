1f1c8b8737f71170653b8f29bdf4265b1adc5ebd
Alex Abreu <alex.abreu@sendgrid.com>
2019-03-03 14:55:33 -0700

    Updates

---
+++ precache-manifest.6374d10ff9e650177ab9b41301a8ac68.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "57365cea434a20b539d8",
+    "url": "/github-prioritizer/static/js/runtime~main.57365cea.js"
+  },
+  {
+    "revision": "29710f95546fdc716485",
+    "url": "/github-prioritizer/static/js/main.29710f95.chunk.js"
+  },
+  {
+    "revision": "053619969b2c17ddf697",
+    "url": "/github-prioritizer/static/js/2.05361996.chunk.js"
+  },
+  {
+    "revision": "db3334146d056529e4588ca79c4bfe4c",
+    "url": "/github-prioritizer/index.html"
+  }
+];
